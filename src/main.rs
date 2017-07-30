2a4fe5b9397c455ccdb0220d1b0847e9bd858847
Harm Berntsen <harm.berntsen@nedap.com>
2017-07-30 08:47:51 +0200

    Current version

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,16 @@ extern crate serde_json;
 extern crate hyper;
 extern crate futures;
 extern crate tokio_core;
-use futures::{Future, Stream};
 use hyper::Client;
+use hyper::mime::{Mime};
 use tokio_core::reactor::Core;
 use std::net::UdpSocket;
 use std::mem;
 use hyper::{Method, Request};
-use hyper::header::{ContentLength, ContentType, Headers};
+use hyper::header::{ContentLength, ContentType};
 header! { (XAuthKey, "X-AUTHKEY") => [String] }
-
-
+use std::str;
+use std::env;
 
 #[derive(Debug)]
 #[repr(C,packed)]
@@ -39,7 +39,7 @@ struct UsageData {
 const USAGEDATA_SIZE: usize = 50;
 
 fn main() {
-    serve().expect("Error");
+    serve().expect("Serve error");
 }
 
 fn to_tm(year: u8, rest: u32) -> Result<time::Tm, time::ParseError> {
@@ -48,7 +48,12 @@ fn to_tm(year: u8, rest: u32) -> Result<time::Tm, time::ParseError> {
         rest_str = format!("0{}", rest_str);
     }
     let time_str = format!("20{}{}", year.to_string(), rest_str);
-    time::strptime(&time_str, "%Y%m%d%H%M%S")
+    let mut time = time::strptime(&time_str, "%Y%m%d%H%M%S")?;
+    // this may generate a few datapoints in the wrong timezone but we can live
+    // with that
+    time.tm_utcoff = time::now().tm_utcoff;
+    time.tm_isdst = time::now().tm_isdst;
+    Ok(time)
 }
 
 fn to_formatted_time(year: u8, rest: u32) -> Result<std::string::String, time::ParseError> {
@@ -64,6 +69,7 @@ fn serve() -> Result<(),std::io::Error> {
     let socket = UdpSocket::bind("0.0.0.0:37678")?;
     let mut core = Core::new().expect("Could not create core");
     let client = Client::new(&core.handle());
+    let authkey = env::var("AUTH_KEY").expect("Please set the AUTH_KEY environment variable");
 
     loop {
         // read from the socket
@@ -75,6 +81,55 @@ fn serve() -> Result<(),std::io::Error> {
         }
 
        let ud: UsageData = unsafe { mem::transmute::<[u8; USAGEDATA_SIZE], UsageData>(buf)};
+       // influxdb
+       {
+
+            {
+                let energy_timestamp = to_tm(ud.timestamp_year, ud.timestamp_rest);
+                if let Err(_) = energy_timestamp {
+                        continue;
+                }
+                let energy_timestamp = energy_timestamp.unwrap();
+
+                let data = format!( "power currently_delivered={},delivered_1={},delivered_2={},currently_phase_l1={},currently_phase_l2={},currently_phase_l3={} {}", ud.power_delivered, ud.energy_delivered_tariff1, ud.energy_delivered_tariff2, ud.power_delivered_l1, ud.power_delivered_l2, ud.power_delivered_l3, energy_timestamp.to_utc().to_timespec().sec);
+                let uri = "http://influxdb:8086/write?db=dsmr&precision=s".parse().unwrap();
+                let mime: Mime = "application/octet-stream".parse().unwrap();
+                let mut req = Request::new(Method::Post, uri);
+                req.headers_mut().set(ContentType(mime));
+                req.headers_mut().set(ContentLength(data.len() as u64));
+                req.set_body(data);
+
+                let post = client.request(req);
+                let res = core.run(post).expect("core run");
+                if res.status() != hyper::StatusCode::NoContent {
+                    println!("POST power: {}", res.status());
+                }
+
+            }
+            {
+                let gas_timestamp = to_tm(ud.gas_timestamp_year, ud.gas_timestamp_rest);
+                if let Err(_) = gas_timestamp {
+                        continue;
+                }
+                let gas_timestamp = gas_timestamp.unwrap();
+            
+                let data = format!( "gas delivered={} {}", ud.gas_delivered, gas_timestamp.to_utc().to_timespec().sec);
+                let uri = "http://influxdb:8086/write?db=dsmr&precision=s".parse().unwrap();
+                let mime: Mime = "application/octet-stream".parse().unwrap();
+                let mut req = Request::new(Method::Post, uri);
+                req.headers_mut().set(ContentType(mime));
+                req.headers_mut().set(ContentLength(data.len() as u64));
+                req.set_body(data);
+
+                let post = client.request(req);
+                let res = core.run(post).expect("core run");
+                if res.status() != hyper::StatusCode::NoContent {
+                    println!("POST gas: {}", res.status());
+                }
+            }
+       }
+
+       // dsmr-reader
        let formatted_timestamp = to_formatted_time(ud.timestamp_year, ud.timestamp_rest);
        if let Err(_) = formatted_timestamp {
             continue;
@@ -100,23 +155,19 @@ fn serve() -> Result<(),std::io::Error> {
             "extra_device_timestamp": formatted_gas_timestamp,
             "extra_device_delivered": format_u32(ud.gas_delivered),
        }).to_string();
-       println!("{}", json);
 
-        let uri = "http://dsmr.aberntsen.nl/api/v2/datalogger/dsmrreading".parse().unwrap();
-        let authkey = String::from("33FS0PT0CL5RY9WRMS0T8TOS01ZUQYO2RXVIRFZSURC4CQQO51GPFACZWB41CD0C");
+        let uri = "http://dsmr-reader/api/v2/datalogger/dsmrreading".parse().unwrap();
         let mut req = Request::new(Method::Post, uri);
         req.headers_mut().set(ContentType::json());
         req.headers_mut().set(ContentLength(json.len() as u64));
-        req.headers_mut().set(XAuthKey(authkey));
+        req.headers_mut().set(XAuthKey(authkey.clone()));
         req.set_body(json);
 
-        let post = client.request(req).and_then(|res| {
-            println!("POST: {}", res.status());
-
-            res.body().concat2()
-        });
-        core.run(post).expect("core run");
-
+        let post = client.request(req);
+        let res = core.run(post).expect("core run");
+        if res.status() != hyper::StatusCode::Created {
+            println!("POST dsmr-reader: {}", res.status());
+        }
     }
 }
 
